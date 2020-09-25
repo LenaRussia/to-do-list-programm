bec740fc4ca919185154de2d644d66f86f1e87ac
LenaRussia <64022741+LenaRussia@users.noreply.github.com>
2020-09-25 12:15:45 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+var list = document.querySelector('.todo-list');
+var items = list.children;
+var toDoListItem = document.querySelectorAll('.todo-list-item');
+var checkBox = document.querySelectorAll('li input');
+var newInput = document.querySelector('#task');
+var submitForm = document.querySelector('.form-flex');
+var submitButton = submitForm.querySelector('.submit-button');
+var newTask = submitForm.querySelector('.todo-list-input');
+var taskTemplate = document.querySelector('#task-template').content;
+var newItemTemplate = taskTemplate.querySelector('.todo-list-item');
+var popap = document.querySelector('.hidden');
+var popapYes = popap.querySelector('.yes');
+var popapNo = popap.querySelector('.no');
+console.log(popapNo);
+
+// delete item 
+var deleteItem = function(item) {
+    var checkbox = item.querySelector('.todo-list-item input');
+    checkbox.addEventListener('change', function() {
+        popap.classList.add('popap');
+        popapNo.addEventListener('click', function() {
+            popap.classList.remove('popap');
+        })
+        popapYes.addEventListener('click', function() {
+            item.remove();
+            popap.classList.remove('popap');
+        })  
+    })
+}
+for (i = 0; i < items.length; i++) {
+    deleteItem(items[i]);
+}
+
+// add item (working only when button pressed, not Enter)
+submitButton.addEventListener('click', function(){
+    var inputText = newTask.value;
+    var task = newItemTemplate.cloneNode(true);
+    var taskDescription = task.querySelector('.todo-list-text-item');
+    taskDescription.textContent = inputText;
+    deleteItem(task);
+    list.appendChild(task);
+    newTask.value = '';
+})
+
+// when clicl on tick, popap window appears
